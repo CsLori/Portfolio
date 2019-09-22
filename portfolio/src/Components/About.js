db98b6e36eb7b8b375390f2601a3558b719298e3
Lorant Csuhai <csuhailori@gmail.com>
2019-09-22 22:19:11 +0100

    Card animation added

--- portfolio/src/Components/About.js
+++ portfolio/src/Components/About.js
@@ -2,42 +2,74 @@ import React from 'react';
 
 const About = () => {
   return (
+    // <>
     <div className="about">
       <h1 className="title">My little techie story</h1>
-      <p className="p">I joined Northcoders in June 2019 as I had been looking for a new challenge and a progressive career
-          change. I have
-          always been interested in the tech industry and I love to read and watch the latest news about it. I started
-          reading
-          about coding bootcamps and after an initial introduction day at Northcoders I was absolutely conviced that
-          coding would
-          be something that I really would like to do on a daily basis and have a career in. Previously I worked in
-          finance, project management and supply chain
-          roles which required adequate Microsoft Excel and problem solving skills. I completed my BSc in Business
-          Administration in Hungary, and my
-          MSc in Oil and Gas Finance in Aberdeen, Scotland but I have to be honest that the 2 degrees combined could not
-          give me as
-        much practical experience and skills as Northcoders has given me.</p>
       <p className="p">
-        Previously I had no coding experience at all, even though I watched a couple of videos years ago, but to be
-        honest that was all. I wish I had been able to do as much
-        data manipulation as I can do now with ease. After completing the enrty challange I
-        started working on my portfolio website,which I have intentionally not updated since to reflect my current level
-        of knowledge firstly because it
-        utilizes Bootstrap, secondly as soon as I finish the course I will build a new one in React. Throughout the
-        course I was lucky enough to work with brilliant people while doing pair progpramming and enjoyed overcoming
-        many challanges arising from learning new things every single day.</p>
-      <p className="p">After completing 3 weeks of fundamentals I
-          really enjoyed back end with all its exciting building for front end, data manipulation and the never ending
-          testing. When I moved on to front end initially I was a bit doubtful that I would not enjoy it as much as I did
-          back end. The beginning was very challenging but as soon as I had a better grasp of it, I began enjoying
-          creating new things or
-          at least something visible. By the time the third 3 week over, I felt a lot more confident working on front
-          end. Currently I am in the project phase and I have been working on a Raspberry Pi project with my team members.
-          We have been through some exciting and also exhausting days due to setting up our back end and Raspberry Pi
-          enviromnent. It took us a couple of days to figure out things and get everything up and running with all the
-          sensors and be able to feed the data to our API. Recently we have been working on front end, initially with just
-        the idea of a responsive web app, but also with the intention of finishing a mobile application. </p>
+        I joined Northcoders in June 2019 as I had been looking for a new
+        challenge and a progressive career change. I have always been interested
+        in the tech industry and I love to read and watch the latest news about
+        it. I started reading about coding bootcamps and after an initial
+        introduction day at Northcoders I was absolutely conviced that coding
+        would be something that I really would like to do on a daily basis and
+        have a career in. Previously I worked in finance, project management and
+        supply chain roles which required adequate Microsoft Excel and problem
+        solving skills. I completed my BSc in Business Administration in
+        Hungary, and my MSc in Oil and Gas Finance in Aberdeen, Scotland but I
+        have to be honest that the 2 degrees combined could not give me as much
+        practical experience and skills as Northcoders has given me.
+      </p>
+      <p className="p">
+        Previously I had no coding experience at all, even though I watched a
+        couple of videos years ago, but to be honest that was all. I wish I had
+        been able to do as much data manipulation as I can do now with ease.
+        After completing the enrty challange I started working on my portfolio
+        website,which I have intentionally not updated since to reflect my
+        current level of knowledge firstly because it utilizes Bootstrap,
+        secondly as soon as I finish the course I will build a new one in React.
+        Throughout the course I was lucky enough to work with brilliant people
+        while doing pair progpramming and enjoyed overcoming many challanges
+        arising from learning new things every single day.
+      </p>
+      <p className="p">
+        After completing 3 weeks of fundamentals I really enjoyed back end with
+        all its exciting building for front end, data manipulation and the never
+        ending testing. When I moved on to front end initially I was a bit
+        doubtful that I would not enjoy it as much as I did back end. The
+        beginning was very challenging but as soon as I had a better grasp of
+        it, I began enjoying creating new things or at least something visible.
+        By the time the third 3 week over, I felt a lot more confident working
+        on front end. Currently I am in the project phase and I have been
+        working on a Raspberry Pi project with my team members. We have been
+        through some exciting and also exhausting days due to setting up our
+        back end and Raspberry Pi enviromnent. It took us a couple of days to
+        figure out things and get everything up and running with all the sensors
+        and be able to feed the data to our API. Recently we have been working
+        on front end, initially with just the idea of a responsive web app, but
+        also with the intention of finishing a mobile application.{' '}
+      </p>
     </div>
+    /* <div>
+    <h2>Layla</h2>
+    <div class="grid">
+      <figure class="effect-layla">
+        <img src="https://tympanus.net/Development/HoverEffectIdeas/img/6.jpg" alt="img06" />
+        <figcaption>
+          <h2>Crazy <span>Layla</span></h2>
+          <p>When Layla appears, she brings an eternal summer along.</p>
+          <a href="#">View more</a>
+        </figcaption>
+      </figure>
+      <figure class="effect-layla">
+        <img src="https://tympanus.net/Development/HoverEffectIdeas/img/3.jpg" alt="img03" />
+        <figcaption>
+          <h2>Crazy <span>Layla</span></h2>
+          <p>When Layla appears, she brings an eternal summer along.</p>
+          <a href="#">View more</a>
+        </figcaption>
+      </figure>
+    </div>
+    </> */
   );
 };
 
--- portfolio/src/Components/Projects.js
+++ portfolio/src/Components/Projects.js
@@ -8,26 +8,38 @@ const Projects = () => {
 
       <div className="projects">
         <div className="row">
-          <div>
+          <div className="box">
             <a href="https://cslori-nc-news.netlify.com/">
               <h4>NC-News</h4>
             </a>
-            <img src="/img/nc-news.jpg" alt="nc-news" />
-            <p></p>
+            <div className="imgBox">
+              <img src="/img/nc-news.jpg" alt="nc-news" />
+            </div>
+            <div className="details">
+              {/* <p>Nc-News</p> */}
+            </div>
           </div>
           <div>
             <a href="https://cslori-nc-news.netlify.com/">
               <h4>TicTacToe Game</h4>
             </a>
-            <img src="/img/cowandchicken.jpg" alt="tictactoe" />
-            <p></p>
+            <div className="imgBox">
+              <img src="/img/cowandchicken.jpg" alt="tictactoe" />
+            </div>
+            <div className="details">
+              {/* <p>CowAndChicken</p> */}
+            </div>
           </div>
           <div>
             <a href="https://cslori-nc-news.netlify.com/">
               <h4>Northcoders Final Project</h4>
             </a>
-            <img src="/img/ReactorGrowFinal2.jpg" alt="finalproject" />
-            <p></p>
+            <div className="imgBox">
+              <img src="/img/ReactorGrowFinal2.jpg" alt="finalproject" />
+            </div>
+            <div className="details">
+              {/* <p>Reator Grow</p> */}
+            </div>
           </div>
         </div>
       </div>
